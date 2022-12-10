c1459f96908920265058a5bebba0bcf1032202fa
u531677 <ANBongioanni@teco.com.ar>
2022-12-10 20:27:58 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,52 +1,59 @@
 import { connectionMysql, connectionSqlite3 } from "./config/connections.js";
 import GestorSqlContainer from "./containers/mysql.container.js";
 
-const gestorProductos = new GestorSqlContainer(connectionMysql, "productos"); //genero instancia del contenedor usando Mysql para la tabla productos;
+async function run() {
+  const gestorProductos = new GestorSqlContainer(connectionMysql, "productos"); //genero instancia del contenedor usando Mysql para la tabla productos;
+  const columnasProductos = {
+    strings: [
+      { name: "imagen", length: 255 },
+      { name: "descripcion", length: 255 },
+      { name: "marca", length: 50 },
+    ],
+    floats: ["precio"],
+    integers: ["stock"],
+  }; //defino las columnas para la tabla productos
 
-const columnasProductos = {
-  strings: [
-    { name: "imagen", length: 255 },
-    { name: "descripcion", length: 255 },
-    { name: "marca", length: 50 },
-  ],
-  floats: ["precio"],
-  integers: ["stock"],
-};
+  //creo la tabla para productos en mysql
+  const respuestaCreacionTablaProductos = await gestorProductos.createTable(
+    columnasProductos
+  );
+  console.log(`${respuestaCreacionTablaProductos} en mysql`);
 
-//creo la tabla para productos en mysql
+  //agrego productos
+  const respuestaProductosAdd = await gestorProductos.addElementos({
+    imagen: "MiFoto",
+    descripcion: "lalalala",
+    marca: "MiMarca",
+    precio: 22.3,
+    stock: 500,
+  });
+  console.log(`producto agregado con id ${respuestaProductosAdd}\n\n`);
 
-gestorProductos.createTable(columnasProductos).then((respuesta) => {
-  console.log(`${respuesta} en mysql`);
+  // =================================TABLA MENSAJES===============================================
 
-  gestorProductos
-    .addElementos({
-      imagen: "MiFoto",
-      descripcion: "lalalala",
-      marca: "MiMarca",
-      precio: 22.3,
-      stock: 500,
-    })
-    .then((respuesta) => console.log(`producto agregado con id ${respuesta}`));
-});
+  const gestorMensajes = new GestorSqlContainer(connectionSqlite3, "mensajes"); //genero una instancia con el mismo modulo, pero para trabajar con msjs
 
-// ================================================================================
-const gestorMensajes = new GestorSqlContainer(connectionSqlite3, "mensajes"); //genero una instancia con el mismo modulo, pero para trabajar con msjs
+  const columnasMensajes = {
+    strings: [
+      { name: "autor", length: 255 },
+      { name: "mensaje", length: 255 },
+    ],
+  }; //defino las columnas para la tabla mensajes
 
-const columnasMensajes = {
-  strings: [
-    { name: "autor", length: 255 },
-    { name: "mensaje", length: 255 },
-  ],
-};
+  //creo la tabla mensajes en sqlite3
+  const respuestaCreacionTablaMsjs = await gestorMensajes.createTable(
+    columnasMensajes
+  );
 
-//creo la tabla mensajes en sqlite3
-gestorMensajes.createTable(columnasMensajes).then((respuesta) => {
-  console.log(`${respuesta} en sqlite3`);
+  console.log(`${respuestaCreacionTablaMsjs} en sqlite3`);
 
-  gestorMensajes
-    .addElementos({
-      autor: "Alejandro",
-      mensaje: "Hola Mundo",
-    })
-    .then((respuesta) => console.log(`mensaje agregado con id ${respuesta}`));
-});
+  //agrego msjs
+  const respuestaMsjsAdd = await gestorMensajes.addElementos({
+    autor: "Alejandro",
+    mensaje: "Hola Mundo",
+  });
+
+  console.log(`mensaje agregado con id ${respuestaMsjsAdd}`);
+}
+
+run();
